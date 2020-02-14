debf1bd3f8300e880e78a64cef841a487c97fb10
victoralbertosg <undacvasg@gmail.com>
2020-02-14 13:03:49 -0500

    update2142

---
+++ UNFV.AnalysisTabular/bkp/consultas.sql
@@ -0,0 +1,27 @@
+USE [NorthwindDataMart]
+GO
+
+/****** Object:  Table [dbo].[DimTime]    Script Date: 14/02/2020 12:45:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[FacMeta](
+	[TimeMetaKey] [int] IDENTITY(1,1) NOT NULL,
+	[dateDT] [datetime]  NULL,
+	[yearDT] [int] NULL,
+	[monthDT] [varchar](20)  NULL,
+	[dayDT] [int]  NULL,
+	metaMount[money] NULL)
+GO
+INSERT INTO [dbo].[FacMeta] ([yearDT],[monthDT],[metaMount])
+select dt.yearDT,dt.monthDT, sum([Total]) from [dbo].[Sales] s
+inner join [dbo].[DimTime] dt on s.dateKey=dt.TimeKey
+group by dt.yearDT,monthDT
+
+truncate table [dbo].[FacMeta]
+
+update [dbo].[FacMeta] set [dateDT]=convert(datetime,(convert(varchar(10),dayDT)) +'/'+convert(varchar(10),monthDT)+'/'+convert(varchar(10),yearDT))
+ 
